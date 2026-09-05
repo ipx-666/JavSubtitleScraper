fb56a306aca3071102c75105d7bfc2972526eb8f
Idea <252221461+ipx-666@users.noreply.github.com>
2026-09-06 01:45:36 +0800

    默认启用字幕扫描任务

--- PluginConfiguration.cs
+++ PluginConfiguration.cs
@@ -4,7 +4,7 @@ namespace JavSubtitleScraper;
 
 public sealed class PluginConfiguration : BasePluginConfiguration
 {
-    public bool EnableScheduledScan { get; set; }
+    public bool EnableScheduledScan { get; set; } = true;
     public bool EnableManualScan { get; set; } = true;
     public bool OverwriteExistingSubtitles { get; set; }
     public string TargetLanguage { get; set; } = "zh-CN";
