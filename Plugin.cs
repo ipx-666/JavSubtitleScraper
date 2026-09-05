719fe71f51acfbb9bb889ae25d7e87a181d0b26d
Idea <252221461+ipx-666@users.noreply.github.com>
2026-09-06 03:33:53 +0800

    增加Emby插件配置页面

--- Plugin.cs
+++ Plugin.cs
@@ -2,10 +2,12 @@ using System;
 using MediaBrowser.Common.Configuration;
 using MediaBrowser.Common.Plugins;
 using MediaBrowser.Model.Serialization;
+using MediaBrowser.Model.Plugins;
+using System.Collections.Generic;
 
 namespace JavSubtitleScraper;
 
-public sealed class Plugin : BasePlugin<PluginConfiguration>
+public sealed class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
 {
     public const string PluginName = "JavSubtitleScraper";
     public static Plugin Instance { get; private set; } = null!;
@@ -19,4 +21,15 @@ public sealed class Plugin : BasePlugin<PluginConfiguration>
     public override Guid Id => new("1fbd6b47-0fa2-4f6e-bc3c-8e8c9b3d0f4d");
     public override string Name => PluginName;
     public override string Description => "Download Chinese subtitles for JAV videos";
+
+    public IEnumerable<PluginPageInfo> GetPages() => new[]
+    {
+        new PluginPageInfo
+        {
+            Name = "JavSubtitleScraperConfig.html",
+            DisplayName = PluginName,
+            EmbeddedResourcePath = "JavSubtitleScraper.Configuration.config.html",
+            IsMainConfigPage = true
+        }
+    };
 }
