7d455b60e2f07809de17cbd05c9bd39a9e8476ac
Idea <252221461+ipx-666@users.noreply.github.com>
2026-09-06 15:50:46 +0800

    按Emby成熟插件模式传递配置页标题

--- Plugin.cs
+++ Plugin.cs
@@ -27,7 +27,6 @@ public sealed class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
         new PluginPageInfo
         {
             Name = Name,
-            DisplayName = "JavSubtitleScraper",
             EmbeddedResourcePath = GetType().Namespace + ".Configuration.config.html",
             EnableInMainMenu = false,
             EnableInUserMenu = false,
